dc711aa83b64576e630a04876a9a79d4b9b505a4
Victor Duman <duman.fabian.g@gmail.com>
2022-03-28 15:01:55 -0500

    primer commit

---
+++ footer.php
@@ -0,0 +1,12 @@
+</div>
+    </main>
+
+    <footer class="text-center my-4">
+        <?php dynamic_sidebar('pie-pagina'); ?>
+    </footer>
+<script src="assets/js/script.js"></script>
+
+<?php wp_footer(); ?>
+</body>
+
+</html>
